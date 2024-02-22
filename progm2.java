9fac0d1327b379ab8db3470d3feb3c6149f70826
bablusi <78784691+bablusi@users.noreply.github.com>
2024-02-23 00:50:57 +0530

    equal or not

---
+++ progm2.java
@@ -0,0 +1,22 @@
+package bablu;
+import java.util.*;
+public class progm2 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the lengt of line a");
+		int a = sc.nextInt();
+		System.out.println("Enter the length of the line b");
+		int b = sc.nextInt();
+		if(a==b)
+		{
+			System.out.println("A and B are equal");	
+		}
+		else
+		{
+			
+			System.out.println("A and B are not equal");
+		}
+	}
+
+}
